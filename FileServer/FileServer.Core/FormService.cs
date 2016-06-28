d2cb86e2b5cd5e9ac7466e81e142490bf58828e9
Shawn Horvatic <SH50169@pekininsurance.com>
2016-06-28 08:53:21 -0500

    Added form

--- FileServer/FileServer.Core/FormService.cs
+++ FileServer/FileServer.Core/FormService.cs
@@ -1,96 +1,122 @@
-﻿//using System;
-//using System.Net;
-//using System.Text;
-//using Server.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Server.Core;
 
-//namespace FileServer.Core
-//{
-//    public class FormService : IHttpServiceProcessor
-//    {
-//        public bool CanProcessRequest(string request, ServerProperties serverProperties)
-//        {
-//            var requestItem = CleanRequest(request);
-//            return requestItem == "/form";
-//        }
+namespace FileServer.Core
+{
+    public class FormService : IHttpServiceProcessor
+    {
+        public bool CanProcessRequest(string request,
+            ServerProperties serverProperties)
+        {
+            var requestItem = CleanRequest(request);
+            return requestItem == "/form";
+        }
 
-//        public IHttpResponse ProcessRequest(string request, IHttpResponse httpResponse,
-//            ServerProperties serverProperties)
-//        {
-//            return request.Contains("GET /form") ? GetRequest(httpResponse) : PostRequest(request, httpResponse);
-//        }
+        public string ProcessRequest(string request,
+            IHttpResponse httpResponse,
+            ServerProperties serverProperties)
+        {
+            return request.Contains("GET /form") ? GetRequest(httpResponse) : PostRequest(request, httpResponse);
+        }
 
-//        private IHttpResponse PostRequest(string request, IHttpResponse httpResponse)
-//        {
-//            var name = request.Remove(0, request.LastIndexOf("\r\n\r\n", StringComparison.Ordinal) + 4);
-//            var firstName = WebUtility.UrlDecode(name.Substring(0, name.IndexOf("&", StringComparison.Ordinal))
-//                .Replace("firstname=", ""));
-//            var lastName =
-//                WebUtility.UrlDecode(name.Substring(name.IndexOf("&", StringComparison.Ordinal) + 1)
-//                    .Replace("lastname=", ""));
+        private string GetRequest(IHttpResponse httpResponse)
+        {
+            var formPage = new StringBuilder();
+            formPage.Append(HtmlHeader());
+            formPage.Append(@"<form action=""form"" method=""post"">");
+            formPage.Append(@"First name:<br>");
+            formPage.Append(@"<input type=""text"" name=""firstname""><br>");
+            formPage.Append(@"Last name:<br>");
+            formPage.Append(@"<input type=""text"" name=""lastname""><br><br>");
+            formPage.Append(@"<input type=""submit"" value=""Submit"">");
+            formPage.Append(@"</form>");
+            formPage.Append(HtmlTail());
+            return SendHeaderAndBody(formPage.ToString(), httpResponse);
+        }
 
-//            var formPage = new StringBuilder();
-//            formPage.Append(@"First Name Submitted:<br>");
-//            formPage.Append(WebUtility.HtmlEncode(firstName) + "<br>");
-//            formPage.Append(@"Last Name Submitted:<br>");
-//            formPage.Append(WebUtility.HtmlEncode(lastName) + "<br>");
+        private string PostRequest(string request, IHttpResponse httpResponse)
+        {
+            var name = request
+                .Remove(0, request.LastIndexOf("\r\n\r\n",
+                    StringComparison.Ordinal) + 4);
+            var firstName = WebUtility
+                .UrlDecode(name.Substring(0, name.IndexOf("&",
+                    StringComparison.Ordinal))
+                    .Replace("firstname=", ""));
+            var lastName =
+                WebUtility.UrlDecode(name.Substring(name
+                    .IndexOf("&", StringComparison.Ordinal) + 1)
+                    .Replace("lastname=", ""));
 
-//            httpResponse.HttpStatusCode = "200 OK";
-//            httpResponse.CacheControl = "no-cache";
-//            httpResponse.ContentType = "text/html";
-//            httpResponse.Body = HtmlHeader() + formPage + HtmlTail();
-//            httpResponse.ContentLength = Encoding
-//                .ASCII.GetByteCount(httpResponse.Body);
-//            return httpResponse;
-//        }
+            var formPage = new StringBuilder();
+            formPage.Append(HtmlHeader());
+            formPage.Append(@"First Name Submitted:<br>");
+            formPage.Append(WebUtility.HtmlEncode(firstName) + "<br>");
+            formPage.Append(@"Last Name Submitted:<br>");
+            formPage.Append(WebUtility.HtmlEncode(lastName) + "<br>");
+            formPage.Append(HtmlTail());
+            return SendHeaderAndBody(formPage.ToString(), httpResponse);
+        }
 
-//        private IHttpResponse GetRequest(IHttpResponse httpResponse)
-//        {
-//            var formPage = new StringBuilder();
-//            formPage.Append(@"<form action=""form"" method=""post"">");
-//            formPage.Append(@"First name:<br>");
-//            formPage.Append(@"<input type=""text"" name=""firstname""><br>");
-//            formPage.Append(@"Last name:<br>");
-//            formPage.Append(@"<input type=""text"" name=""lastname""><br><br>");
-//            formPage.Append(@"<input type=""submit"" value=""Submit"">");
-//            formPage.Append(@"</form>");
-//            httpResponse.HttpStatusCode = "200 OK";
-//            httpResponse.CacheControl = "no-cache";
-//            httpResponse.ContentType = "text/html";
-//            httpResponse.Body = HtmlHeader() + formPage + HtmlTail();
-//            httpResponse.ContentLength = Encoding
-//                .ASCII.GetByteCount(httpResponse.Body);
-//            return httpResponse;
-//        }
+        private string SendHeaderAndBody(string formPage, 
+            IHttpResponse httpResponse)
+        {
+            httpResponse.SendHeaders(new List<string>
+            {
+                "HTTP/1.1 200 OK\r\n",
+                "Cache-Control: no-cache\r\n",
+                "Content-Type: text/html\r\n",
+                "Content-Length: "
+                + (GetByteCount(formPage.ToString())) +
+                "\r\n\r\n"
+            });
+            httpResponse.SendBody(GetByte(formPage.ToString()),
+                GetByteCount(formPage.ToString()));
+            return "200 OK";
+        }
 
-//        private string CleanRequest(string request)
-//        {
-//            var parseVaulue = request.Contains("GET") ? "GET" : "POST";
-//            var offsets = request.Contains("GET") ? 5 : 6;
-//            if (request.Contains("HTTP/1.1"))
-//                return "/" + request.Substring(request.IndexOf(parseVaulue + " /", StringComparison.Ordinal) + offsets,
-//                    request.IndexOf(" HTTP/1.1", StringComparison.Ordinal) - offsets)
-//                    .Replace("%20", " ");
-//            return "/" + request.Substring(request.IndexOf(parseVaulue + " /", StringComparison.Ordinal) + offsets,
-//                request.IndexOf(" HTTP/1.0", StringComparison.Ordinal) - offsets)
-//                .Replace("%20", " ");
-//        }
+        private string CleanRequest(string request)
+        {
+            var parseVaulue = request.Contains("GET") ? "GET" : "POST";
+            var offsets = request.Contains("GET") ? 5 : 6;
+            if (request.Contains("HTTP/1.1"))
+                return "/" + request.Substring(request.IndexOf(parseVaulue + " /", StringComparison.Ordinal) + offsets,
+                    request.IndexOf(" HTTP/1.1", StringComparison.Ordinal) - offsets)
+                    .Replace("%20", " ");
+            return "/" + request.Substring(request.IndexOf(parseVaulue + " /", StringComparison.Ordinal) + offsets,
+                request.IndexOf(" HTTP/1.0", StringComparison.Ordinal) - offsets)
+                .Replace("%20", " ");
+        }
 
-//        private string HtmlHeader()
-//        {
-//            var header = new StringBuilder();
-//            header.Append(@"<!DOCTYPE html>");
-//            header.Append(@"<html>");
-//            header.Append(@"<head><title>Vatic Form Page</title></head>");
-//            header.Append(@"<body>");
-//            return header.ToString();
-//        }
+        private string HtmlHeader()
+        {
+            var header = new StringBuilder();
+            header.Append(@"<!DOCTYPE html>");
+            header.Append(@"<html>");
+            header.Append(@"<head><title>Vatic Form Page</title></head>");
+            header.Append(@"<body>");
+            return header.ToString();
+        }
 
-//        private string HtmlTail()
-//        {
-//            var tail = new StringBuilder();
-//            tail.Append(@"</body>");
-//            tail.Append(@"</html>");
-//            return tail.ToString();
-//        }
-//    }
-//}
+        private string HtmlTail()
+        {
+            var tail = new StringBuilder();
+            tail.Append(@"</body>");
+            tail.Append(@"</html>");
+            return tail.ToString();
+        }
+
+        private int GetByteCount(string message)
+        {
+            return Encoding.ASCII.GetByteCount(message);
+        }
+
+        private byte[] GetByte(string message)
+        {
+            return Encoding.ASCII.GetBytes(message);
+        }
+    }
+}
--- FileServer/FileServer.Test/FormServiceTest.cs
+++ FileServer/FileServer.Test/FormServiceTest.cs
@@ -1,125 +1,170 @@
-﻿//using System.Text;
-//using FileServer.Core;
-//using Server.Core;
-//using Xunit;
+﻿using System.Text;
+using FileServer.Core;
+using Server.Core;
+using Xunit;
 
-//namespace FileServer.Test
-//{
-//    public class FormServiceTest
-//    {
-//        [Fact]
-//        public void Make_Not_Null_Class()
-//        {
-//            Assert.NotNull(new FormService());
-//        }
+namespace FileServer.Test
+{
+    public class FormServiceTest
+    {
+        [Fact]
+        public void Make_Not_Null_Class()
+        {
+            Assert.NotNull(new FormService());
+        }
 
-//        [Theory]
-//        [InlineData("GET /form HTTP/1.1")]
-//        [InlineData("GET /form HTTP/1.0")]
-//        [InlineData("POST /form HTTP/1.1")]
-//        [InlineData("POST /form HTTP/1.0")]
-//        public void Can_Process(string getRequest)
-//        {
-//            var mockFileSearch = new MockFileProcessor();
-//            mockFileSearch.StubExists(true);
-//            var properties = new ServerProperties(@"c:/",
-//                5555, new HttpResponse(), new ServerTime(),
-//                new MockPrinter(),
-//                new Readers
-//                {
-//                    DirectoryProcess = new DirectoryProcessor(),
-//                    FileProcess = mockFileSearch
-//                });
-//            var formService = new FormService();
+        [Theory]
+        [InlineData("GET /form HTTP/1.1")]
+        [InlineData("GET /form HTTP/1.0")]
+        [InlineData("POST /form HTTP/1.1")]
+        [InlineData("POST /form HTTP/1.0")]
+        public void Can_Process(string getRequest)
+        {
+            var mockFileSearch = new MockFileProcessor()
+                .StubExists(true);
+            var properties = new ServerProperties(@"c:/",
+                5555, new ServerTime(),
+                new MockPrinter(),
+                new Readers
+                {
+                    DirectoryProcess = new DirectoryProcessor(),
+                    FileProcess = mockFileSearch
+                });
+            var formService = new FormService();
 
-//            Assert.True(formService
-//                .CanProcessRequest(getRequest, properties));
-//        }
+            Assert.True(formService
+                .CanProcessRequest(getRequest, properties));
+        }
 
-//        [Fact]
-//        public void Send_Data_Get_Request()
-//        {
-//            var mockFileSearch = new MockFileProcessor();
-//            mockFileSearch.StubExists(true);
-//            var properties = new ServerProperties(@"c:/",
-//                5555, new HttpResponse(), new ServerTime(),
-//                new MockPrinter(),
-//                new Readers
-//                {
-//                    DirectoryProcess = new DirectoryProcessor(),
-//                    FileProcess = mockFileSearch
-//                });
-//            var formService = new FormService();
+        [Fact]
+        public void Send_Data_Get_Request()
+        {
+            var zSocket = new MockZSocket();
+            var mockFileSearch = new MockFileProcessor()
+                .StubExists(true);
+            var properties = new ServerProperties(@"c:/",
+                5555, new ServerTime(),
+                new MockPrinter(),
+                new Readers
+                {
+                    DirectoryProcess = new DirectoryProcessor(),
+                    FileProcess = mockFileSearch
+                });
+            var formService = new FormService();
 
-//            var httpResponces = formService
-//                .ProcessRequest("GET /form HTTP/1.1",
-//                    new HttpResponse(), properties);
+            var statusCode = formService
+                .ProcessRequest("GET /form HTTP/1.1",
+                    new HttpResponse(zSocket), properties);
 
-//            var correctOutput = new StringBuilder();
-//            correctOutput.Append(@"<!DOCTYPE html>");
-//            correctOutput.Append(@"<html>");
-//            correctOutput.Append(@"<head><title>Vatic Form Page</title></head>");
-//            correctOutput.Append(@"<body>");
-//            correctOutput.Append(@"<form action=""form"" method=""post"">");
-//            correctOutput.Append(@"First name:<br>");
-//            correctOutput.Append(@"<input type=""text"" name=""firstname""><br>");
-//            correctOutput.Append(@"Last name:<br>");
-//            correctOutput.Append(@"<input type=""text"" name=""lastname""><br><br>");
-//            correctOutput.Append(@"<input type=""submit"" value=""Submit"">");
-//            correctOutput.Append(@"</form>");
-//            correctOutput.Append(@"</body>");
-//            correctOutput.Append(@"</html>");
+            var correctOutput = new StringBuilder();
+            correctOutput.Append(@"<!DOCTYPE html>");
+            correctOutput.Append(@"<html>");
+            correctOutput.Append(@"<head><title>Vatic Form Page</title></head>");
+            correctOutput.Append(@"<body>");
+            correctOutput.Append(@"<form action=""form"" method=""post"">");
+            correctOutput.Append(@"First name:<br>");
+            correctOutput.Append(@"<input type=""text"" name=""firstname""><br>");
+            correctOutput.Append(@"Last name:<br>");
+            correctOutput.Append(@"<input type=""text"" name=""lastname""><br><br>");
+            correctOutput.Append(@"<input type=""submit"" value=""Submit"">");
+            correctOutput.Append(@"</form>");
+            correctOutput.Append(@"</body>");
+            correctOutput.Append(@"</html>");
 
-//            Assert.Equal(correctOutput.ToString(), httpResponces.Body);
-//        }
+            Assert.Equal("200 OK", statusCode);
+            zSocket.VerifySend(GetByte("HTTP/1.1 200 OK\r\n"),
+                GetByteCount("HTTP/1.1 200 OK\r\n"));
+            zSocket.VerifySend(GetByte("Cache-Control: no-cache\r\n"),
+                GetByteCount("Cache-Control: no-cache\r\n"));
+            zSocket.VerifySend(GetByte("Content-Type: text/html\r\n"),
+                GetByteCount("Content-Type: text/html\r\n"));
+            zSocket.VerifySend(GetByte("Content-Length: "
+                                       + GetByteCount(correctOutput.ToString())
+                                       + "\r\n\r\n"),
+                GetByteCount("Content-Length: "
+                             + GetByteCount(correctOutput.ToString())
+                             + "\r\n\r\n"));
+            zSocket.VerifySend(GetByte(correctOutput.ToString()),
+                GetByteCount(correctOutput.ToString()));
+            zSocket.VerifySend(GetByte(correctOutput.ToString()),
+                GetByteCount(correctOutput.ToString()));
+        }
 
-//        [Fact]
-//        public void Send_Data_Post_Request()
-//        {
-//            var mockFileSearch = new MockFileProcessor();
-//            mockFileSearch.StubExists(true);
-//            var properties = new ServerProperties(@"c:/",
-//                5555, new HttpResponse(), new ServerTime(),
-//                new MockPrinter(),
-//                new Readers
-//                {
-//                    DirectoryProcess = new DirectoryProcessor(),
-//                    FileProcess = mockFileSearch
-//                });
-//            var formService = new FormService();
+        [Fact]
+        public void Send_Data_Post_Request()
+        {
+            var zSocket = new MockZSocket();
+            var mockFileSearch = new MockFileProcessor();
+            mockFileSearch.StubExists(true);
+            var properties = new ServerProperties(@"c:/",
+                5555, new ServerTime(),
+                new MockPrinter(),
+                new Readers
+                {
+                    DirectoryProcess = new DirectoryProcessor(),
+                    FileProcess = mockFileSearch
+                });
+            var formService = new FormService();
 
-//            var httpResponces = formService.ProcessRequest("POST /form HTTP/1.1\r\n" +
-//                                                           "Host: localhost:8080\r\n" +
-//                                                           "Connection: keep-alive\r\n" +
-//                                                           "Content-Length: 33\r\n" +
-//                                                           "Cache - Control: max - age = 0\r\n" +
-//                                                           "Accept: text / html,application / xhtml + xml,application / xml; q = 0.9,image / webp,*/*;q=0.8\r\n" +
-//                                                           "Origin: http://localhost:8080\r\n" +
-//                                                           "Upgrade-Insecure-Requests: 1\r\n" +
-//                                                           "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36\r\n" +
-//                                                           "Content-Type: application/x-www-form-urlencoded\r\n" +
-//                                                           "Referer: http://localhost:8080/form\r\n" +
-//                                                           "Accept-Encoding: gzip, deflate\r\n" +
-//                                                           "Accept-Language: en-US,en;q=0.8\r\n\r\n" +
-//                                                           "firstname=John%26"
-//                                                           + "&lastname=Walsher%26", new HttpResponse(), properties);
+            var statusCode = formService.ProcessRequest("POST /form HTTP/1.1\r\n" +
+                                                        "Host: localhost:8080\r\n" +
+                                                        "Connection: keep-alive\r\n" +
+                                                        "Content-Length: 33\r\n" +
+                                                        "Cache - Control: max - age = 0\r\n" +
+                                                        "Accept: text / html,application / xhtml + xml,application / xml; q = 0.9,image / webp,*/*;q=0.8\r\n" +
+                                                        "Origin: http://localhost:8080\r\n" +
+                                                        "Upgrade-Insecure-Requests: 1\r\n" +
+                                                        "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36\r\n" +
+                                                        "Content-Type: application/x-www-form-urlencoded\r\n" +
+                                                        "Referer: http://localhost:8080/form\r\n" +
+                                                        "Accept-Encoding: gzip, deflate\r\n" +
+                                                        "Accept-Language: en-US,en;q=0.8\r\n\r\n" +
+                                                        "firstname=John%26"
+                                                        + "&lastname=Walsher%26",
+                new HttpResponse(zSocket), properties);
 
-//            var correctOutput = new StringBuilder();
-//            correctOutput.Append(@"<!DOCTYPE html>");
-//            correctOutput.Append(@"<html>");
-//            correctOutput.Append(@"<head><title>Vatic Form Page</title></head>");
-//            correctOutput.Append(@"<body>");
+            var correctOutput = new StringBuilder();
+            correctOutput.Append(@"<!DOCTYPE html>");
+            correctOutput.Append(@"<html>");
+            correctOutput.Append(@"<head><title>Vatic Form Page</title></head>");
+            correctOutput.Append(@"<body>");
 
-//            correctOutput.Append(@"First Name Submitted:<br>");
-//            correctOutput.Append(@"John&amp;<br>");
-//            correctOutput.Append(@"Last Name Submitted:<br>");
-//            correctOutput.Append(@"Walsher&amp;<br>");
+            correctOutput.Append(@"First Name Submitted:<br>");
+            correctOutput.Append(@"John&amp;<br>");
+            correctOutput.Append(@"Last Name Submitted:<br>");
+            correctOutput.Append(@"Walsher&amp;<br>");
 
 
-//            correctOutput.Append(@"</body>");
-//            correctOutput.Append(@"</html>");
+            correctOutput.Append(@"</body>");
+            correctOutput.Append(@"</html>");
 
-//            Assert.Equal(correctOutput.ToString(), httpResponces.Body);
-//        }
-//    }
-//}
+            Assert.Equal("200 OK", statusCode);
+            zSocket.VerifySend(GetByte("HTTP/1.1 200 OK\r\n"),
+                GetByteCount("HTTP/1.1 200 OK\r\n"));
+            zSocket.VerifySend(GetByte("Cache-Control: no-cache\r\n"),
+                GetByteCount("Cache-Control: no-cache\r\n"));
+            zSocket.VerifySend(GetByte("Content-Type: text/html\r\n"),
+                GetByteCount("Content-Type: text/html\r\n"));
+            zSocket.VerifySend(GetByte("Content-Length: "
+                                       + GetByteCount(correctOutput.ToString())
+                                       + "\r\n\r\n"),
+                GetByteCount("Content-Length: "
+                             + GetByteCount(correctOutput.ToString())
+                             + "\r\n\r\n"));
+            zSocket.VerifySend(GetByte(correctOutput.ToString()),
+                GetByteCount(correctOutput.ToString()));
+            zSocket.VerifySend(GetByte(correctOutput.ToString()),
+                GetByteCount(correctOutput.ToString()));
+        }
+
+        private int GetByteCount(string message)
+        {
+            return Encoding.ASCII.GetByteCount(message);
+        }
+
+        private byte[] GetByte(string message)
+        {
+            return Encoding.ASCII.GetBytes(message);
+        }
+    }
+}
